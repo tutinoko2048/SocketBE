553ea53f772b8ae46d58275246a2d8fba8526f78
Retoruto9900K <tutinoko2048@gmail.com>
2023-04-18 08:05:08 +0000

    typing: use of constant in ServerEvents

--- typings/types.d.ts
+++ typings/types.d.ts
@@ -46,30 +46,31 @@ export interface LoggerOption {
 }
 
 export interface ServerEvents {
-  playerJoin:  { players: string[], world: World },
-  playerLeave: { players: string[], world: World }
-  serverOpen: void,
-  serverClose: void,
-  worldAdd: { world: World },
-  worldRemove: { world: World },
-  packetReceive: { packet: any, world: World },
-  error: Error,
-  playerChat: {
+  [Events.PlayerJoin]:  { players: string[], world: World },
+  [Events.PlayerLeave]: { players: string[], world: World }
+  [Events.ServerOpen]: void,
+  [Events.ServerClose]: void,
+  [Events.WorldAdd]: { world: World },
+  [Events.WorldRemove]: { world: World },
+  [Events.PacketReceive]: { packet: any, world: World },
+  [Events.Error]: Error,
+  [Events.PlayerChat]: {
     'type': 'chat' | 'say' | 'me' | 'tell',
     message: string,
     sender: string,
     receiver: string,
     world: World
   },
-  playerTitle: {
+  [Events.PlayerTitle]: {
     'type': 'title',
     message: string,
     sender: string,
     receiver: string,
     world: World
   },
-  tick: void
+  [Events.Tick]: void
 }
 
 import World = require("./structures/World");
+import Events = require("./util/Events");
 import WebSocket from 'ws';
