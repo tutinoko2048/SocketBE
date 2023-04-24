7fa78dd3fab57340cbbbbdc57c9b973714b75006
Retoruto9900K <tutinoko2048@gmail.com>
2023-04-24 03:47:37 +0000

    typing: VersionResolvable in Util.d.ts

--- typings/util/Util.d.ts
+++ typings/util/Util.d.ts
@@ -3,7 +3,7 @@ declare class Util {
     static isEmpty(v: any): boolean;
     static getTime(timezone: string, mode?: string): string;
     static eventBuilder(eventName: string, eventPurpose?: 'subscribe' | 'unsubscribe'): any;
-    static commandBuilder(cmd: string, commandVersion?: number): any;
+    static commandBuilder(cmd: string, commandVersion?: import('../types').VersionResolvable): any;
     static splitNicely(str: string): string;
     static median(numbers: number[]): number;
     static average(numbers: number[]): number;
